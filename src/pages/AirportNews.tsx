532144970b35b3ee7350d156d75e1a00ea8cc3d5
Heesu01 <rulru0413@naver.com>
2025-02-05 23:16:34 +0900

    setting: 프로젝트 구조 설정

---
+++ src/pages/AirportNews.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AirportNews = () => {
+  return <div></div>;
+};
+
+export default AirportNews;
---
+++ src/pages/RealTimeAirport.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RealTimeAirport = () => {
+  return <div></div>;
+};
+
+export default RealTimeAirport;
